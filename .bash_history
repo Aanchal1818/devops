dcc172d0faef0c16781b35771a0b422f4c73cea2
Aanchal <aanchaljogeshwar1@gmail.com>
2025-03-15 02:58:16 +0000

    first commit

---
+++ .bash_history
@@ -0,0 +1,12 @@
+clear
+cat /etc/os-release
+history
+vim script1.sh
+chmod +x script1.sh
+./script.sh
+./script1.sh
+cat script1.sh
+vi script1.sh
+vim script1.sh
+clear
+sudo su -
---
+++ script1.sh
@@ -0,0 +1,232 @@
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
+#! /bin/bash
+
+echo "downloading packages"
+sudo apt update
+sudo apt install nano -y
+sudo apt install git -y
+
